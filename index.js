3cb3e61532f2fe662fb0b1a8fc32cd7a34cc46be
Hotai04 <bendadavide@gmail.com>
2018-04-24 15:25:18 +0200

    Aggiunta gestione invio mail

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var nodemailer = require('nodemailer');
 var app = express();
-
-app.set('port', (process.env.PORT || 3000));
+//Gestione dati statici (immagini, css)
 app.use(express.static('public'));
+//Gestione pagine da visualizzare e strumento di render
 app.set('views', __dirname + '/FrontEnd');
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//Gestione informazioni url( login, ricerca)
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Carico pagina principale
@@ -16,13 +17,7 @@ app.get('/', function( req, res)
     res.render("login.html");
 });
 
-/*
-app.get('/home', function(req, res)
-{
-    console.log("GET HOME");
-    res.render('home.html');
-});*/
-
+//Accesso home mediante POST
 app.post('/home', function(req, res)
 {
     var data = req.body.username + " " + req.body.password;
@@ -31,50 +26,84 @@ app.post('/home', function(req, res)
     res.render('home.html');
 });
 
+//Accesso home mediante GET
+app.get('/home', function(req, res)
+{
+    console.log("GET HOME");
+    res.render('home.html');
+});
+
+//Accesso alla zona segreteria
 app.get('/segreteria', function(req,res)
 {
     console.log("GET SEGRETERIA");
     res.render("segreteria.html");
 });
 
-/*app.get('/topic/erasmus', function(req,res)
+//Invio mail per la segreteria
+app.post('/send', function(req, res)
 {
-    console.log("GET ERASMUMS");
-    res.render("topic/erasmus.html");
+    //console.log(req.body.mesg);
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "se2testingprj1@gmail.com",// generated ethereal user
+            pass: "w6tc+5C]" // generated ethereal password
+        },
+        tls:{
+            rejectUnauthorized:false
+        }
+    });
+    // setup email data with unicode symbols
+    var mailOptions = {
+        from: '"Test" <se2testingprj@gmail.com>', // sender address
+        to: 'bendadavide@gmail.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: req.body.mesg // plain text body
+        //html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function (error, info)
+    {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+    res.redirect("/home");
 });
 
-app.get('/topic/laurea', function(req,res)
-{
-    console.log("GET LAUREA");
-    res.render("topic/laurea.html");
-});
-
-app.get('/topic/tasse', function(req,res)
-{
-    console.log("GET TASSE");
-    res.render("topic/tasse.html");
-});
-
-app.get('/topic/tirocini', function(req,res)
-{
-    console.log("GET TIROCINI");
-    res.render("topic/tirocini.html");
-});*/
+//Gestione ricerca
 app.get('/topic/:questionValue', function(req, res)
 {
-        console.log("GET " + req.params.questionValue);
+
+        //console.log("GET " + req.params.questionValue);
+
+        //Pagina html da ricercare in base al valore inserito
         var topic = req.params.questionValue+".html";
+        //Render pagina
         res.render(("topic/"+ topic),function (err, html)
             {
+                //Se non esiste restituisce una pagina di errore
                 if(err)
                 {
                     res.render("segreteria.html");
                 }
-                res.send(html);
+                //Se esiste viene visualizzata
+                res.render(html);
             }
         );
 });
 
+//Funzione di ascolto
 app.listen(3000, function () {
     console.log('Example app listening on port 3000.');
 });
