76f3ea7b30d842a53365ca29f60388457e02e2e0
Hotai04 <bendadavide@gmail.com>
2018-05-11 16:13:11 +0200

    Aggiornamento backend con le pagine principali responsive

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var nodemailer = require('nodemailer');
+var expressSes = require('express-session');
 var vectorUsr = ['se2testingprj1@gmail.com','aaa'], vectorPswrd = ['w6tc+5C','aaa'];
 var app = express();
-var loggedIn = false;
+
 //Gestione dati statici (immagini, css)
 app.use(express.static('public'));
 //Gestione pagine da visualizzare e strumento di render
 app.set('views', __dirname + '/FrontEnd');
-app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs');
+
 //Gestione informazioni url( login, ricerca)
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(expressSes({secret: 'gS3NyHfq70', saveUninitialized: false, resave: false}));
+
 //Settaggio porta per gestione sia locale che su heroku
 app.set('port', process.env.PORT || 3000);
 // Carico pagina principale
 app.get('/', function( req, res)
 {
     console.log("GET LOGIN");
-    res.render("login.html");
+    res.render('login', {success: req.session.success});
 });
 
 //Accesso home mediante POST
@@ -25,51 +31,47 @@ app.post('/home', function(req, res)
 {
     if( (vectorUsr.indexOf(req.body.username)!= -1) && (vectorPswrd.indexOf(req.body.password) != -1))
     {
-        loggedIn=true;
+        req.session.success=true;
         console.log(req.body.username + ' submitted Successfully!');
         console.log("POST HOME");
-        res.status(200).render('home.html');
+        res.status(200).render('home',{success: req.session.success});
 
     }
     else
     {
-        res.status(404).send();
+        req.session.success = false;
+        res.status(403).redirect('/');
     }
 });
-
-function checkLogin()
-{
-    return loggedIn;
-}
 
 //Accesso home mediante GET
 app.get('/home', function(req, res)
 {
-    if(!loggedIn)
+    if(req.session.success)
     {
-        res.status(403).send("<h1> Area non autorizzata!</h1>");
+        console.log("GET HOME");
+        res.status(200).render('home',{success: req.session.success});
     }
     else
     {
-        console.log("GET HOME");
-        res.status(200).render('home.html');
+        console.log("GET LOGIN");
+        res.status(403).redirect('/');
     }
-
 });
 
 //Accesso alla zona segreteria
 app.get('/segreteria', function(req,res)
 {
-    if(!loggedIn)
+    if(req.session.success)
     {
-        res.status(403).send("<h1> Area non autorizzata!</h1>");
+        console.log("GET SEGRETERIA");
+        res.status(200).render("segreteria",{success: req.session.success});
     }
     else
     {
-        console.log("GET SEGRETERIA");
-        res.status(200).render("segreteria.html");
+        console.log("GET LOGIN");
+        res.status(403).redirect('/');
     }
-
 
 });
 
@@ -105,12 +107,40 @@ app.post('/send', function(req, res)
             return console.log(error);
         }
     });
-    res.redirect("/home");  //Ritorno alla home dopo l'invio del messaggio
+    res.redirect("home");  //Ritorno alla home dopo l'invio del messaggio
 });
 
 //Gestione ricerca
 app.get('/topic/:questionValue', function(req, res)
     {
+        var error =false;
+        if(req.session.success)
+        {
+            //Pagina html da ricercare in base al valore inserito
+            var topic = req.params.questionValue.toLowerCase();
+            //Render pagina
+            res.render(('topic/'+ topic),function (err)
+            {
+                //Se non esiste restituisce una pagina di errore
+                if(err)
+                {
+                    error= true;
+                    console.log("GET PAGINA NON TROVATA");
+                    res.status(404).render('pag_non_trovata',{success: req.session.success});
+                }
+            });
+            if(!error)
+            {
+              console.log("GET " + req.params.questionValue.toUpperCase());
+              res.status(200).render('topic/'+ topic,{success: req.session.success});
+            }
+        }
+        else
+        {
+            console.log("GET LOGIN");
+            res.status(403).redirect('/');
+        }
+        /*
         if(!loggedIn)
         {
             res.status(403).send("<h1> Area non autorizzata!</h1>");
@@ -136,7 +166,7 @@ app.get('/topic/:questionValue', function(req, res)
                     }
                 }
             );
-        }
+        }*/
 
 });
 //Funzione di ascolto
---
+++ public/script/pathManager.js
@@ -0,0 +1,37 @@
+//Funzione per gestione barra di ricerca
+function searchBar()
+{
+    //Variabile che gestisce il path
+    var response;
+    //Verifico che la pagina da cui effettuo la ricerca sia la home
+    if(document.body.contains(document.getElementById('homeSearch')))
+    {
+        //Cerco il file accedendo alla cartella topic
+        response ='topic/'+document.getElementById('inputLGEx').value.toLowerCase();
+    }
+    else
+    {
+        //Cerco il file senza accesso (ricerca effettuata da pag_non_trovata)
+        response = document.getElementById('inputLGEx').value.toLowerCase();
+    }
+
+    //Reindirizzamento alla pagina corrispondente (pagine inesistenti gestite dal server)
+    location = response;
+    return false;
+}
+
+//Funzione per la gestione dei bottoni per reindirizzamento diretto alle pagine più cercate
+function buttonBar()
+{
+    //Lettura del form dove inserire l'ascoltatore
+    var form = document.getElementById('buttons');
+    //Ascoltatore che individua il bottone preso
+    var listener = function (e)
+    {
+        //Reindirizzamento alla pagina in base al testo del bottone (pagine inesistenti gestite dal server)
+        location = 'topic/'+e.target.value.toLowerCase();
+    };
+    //Aggiunta ascoltatore
+    form.addEventListener('click', listener, false);
+    return false;
+}
